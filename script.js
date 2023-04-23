0540b063a95e00c8002740db9b0c464468addd19
Akshay Nagar <118043847+akshaynagar14@users.noreply.github.com>
2023-04-23 18:07:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  // Get a reference to the body tag
+  const body = document.querySelector("body");
+
+  // Create a new text node with the message
+  const message = document.createTextNode("DOM load success");
+
+  // Append the text node to the body tag
+  body.appendChild(message);
